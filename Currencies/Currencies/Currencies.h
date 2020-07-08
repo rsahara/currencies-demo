3efa9822fd54949399e74c6b2f022aa218d76a3c
Runo Sahara <runo.sahara@linecorp.com>
2020-07-08 12:40:14 +0900

    Empty app

---
+++ Currencies/Currencies/Currencies.h
@@ -0,0 +1,19 @@
+//
+//  Currencies.h
+//  Currencies
+//
+//  Created by Sahara Runo on 2020/07/08.
+//  Copyright © 2020 Runo Sahara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Currencies.
+FOUNDATION_EXPORT double CurrenciesVersionNumber;
+
+//! Project version string for Currencies.
+FOUNDATION_EXPORT const unsigned char CurrenciesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Currencies/PublicHeader.h>
+
+
---
+++ CurrenciesApp/CurrenciesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrenciesApp
+//
+//  Created by Sahara Runo on 2020/07/08.
+//  Copyright © 2020 Runo Sahara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
